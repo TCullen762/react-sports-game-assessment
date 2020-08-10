6f5749dcc29e076ef61f2759fdd05583a1f70bef
Timothy Cullen <cullen762@Timothys-MacBook-Air.local>
2020-08-10 18:49:51 -0400

    grrr

--- src/App.js
+++ src/App.js
@@ -5,14 +5,40 @@ import Game from'./components/game/game.js';
 import Team from './components/team/team.js';
 import Score from './components/scoreboard/scoreboard.js';
 
-function App() {
-  return (
+function App (props) {
+  const rebels = {
+      name: 'Cork Town Rebels',
+      logoSrc:'/Users/cullen762/Documents/assessments/reactBasics_sportsGame/assets/th-1.jpg'
+  }
+
+  const rats = {
+      name: 'Seattle Street Rats',
+      logoSrc:'/Users/cullen762/Documents/assessments/reactBasics_sportsGame/assets/rat.jpg'
+  }
+
+  const karen = {
+      name: 'California Karens',
+      logoSrc:'/Users/cullen762/Documents/assessments/reactBasics_sportsGame/assets/download-2.jpg'
+
+  }
+  const retail ={
+      name:'battered baristas',
+      logoSrc:'/Users/cullen762/Documents/assessments/reactBasics_sportsGame/assets/retail.jpg'
+  }
+
+return (
     <div className="App">
-      <Game/>
-      <Team/>
-      < Score/>
-    </div>
-  );
+      <Game venue=' Ikea arena'
+      homeTeam={rats}
+      visitingTeam={rebels}
+      />
+      <Game venue='corporate branded arena'
+      homeTeam ={retail}
+      visitingTeam={karen}
+      />
+     
+  </div>
+)
 }
 
 export default App;
--- src/components/game/game.js
+++ src/components/game/game.js
@@ -1,19 +1,10 @@
 
 import React from 'react';
-function Game (props){
-    return(
-        <div className='game'>
-            Game Component
-        </div>
-
-    )
-
-}
-
-export default Game;
+import Team from '../team/team.js';
 
 
-/*class Game extends React.Component{
+
+class Game extends React.Component{
     constructor(props){
         super(props)
         this.state = {
@@ -29,22 +20,22 @@ export default Game;
 
         }
 
-        this.shotSound = new Audio('/Users/cullen762/Documents/assessments/reactBasics_sportsGame/assets/bat+hit+ball.wav')
-        this.scoreSound = new Audio('/Users/cullen762/Documents/assessments/reactBasics_sportsGame/assets/cheers.wav')
+        /*this.shotSound = new Audio('/Users/cullen762/Documents/assessments/reactBasics_sportsGame/assets/bat+hit+ball.wav')
+        this.scoreSound = new Audio('/Users/cullen762/Documents/assessments/reactBasics_sportsGame/assets/cheers.wav')*/
 
         
     }
     shotHandler =(team) => {
-        const teamStatsKey = '${team}TeamStats'
+        /*const teamStatsKey = '${team}TeamStats'*/
         let score = this.state.score
-        this.shotSound.play()
+        /*this.shotSound.play()*/
         
         if (Math.random() > 0.5){
             score += 1
 
-            setTimeout(() => {
+          /*  setTimeout(() => {
                 this.scoreSound.play("/Users/cullen762/Documents/assessments/reactBasics_sportsGame/assets/cheers.wav")
-            }, 1000)
+            }, 1000)*/
             
      }
         this.setState((state,props) =>({
@@ -57,12 +48,12 @@ export default Game;
 render(){
     return (
 <div className = "Game">
-<h1>Welcome to {props.venue} </h1>
+<h1>Welcome to {this.props.venue} </h1>
 <div className="stats">
     <div>
       <Team 
-      name = {props.visitingTeam.name}
-      logo = {props.visitingTeam.logoSrc}
+      name = {this.props.visitingTeam.name}
+      logo = {this.props.visitingTeam.logoSrc}
       stats = {this.state.visitingTeamStats}
        /> 
       <br/>
@@ -70,8 +61,7 @@ render(){
       <div className="versus">
       <h1>VS!</h1></div>
       <Team 
-      name = {props.homeTeam.name}
-      logo ={props.homeTeam.logoSrc}
+      logo ={this.props.homeTeam.logoSrc}
       stats ={this.state.homeTeamStats}
        /> 
        </div>
@@ -80,4 +70,8 @@ render(){
 )
 
 }
-}*/
+}
+
+export default Game;
+
+
